dc84b4b3da76aa193ae488869fc458e2307b3bbc
Christian <christianson.gruenberg@gmail.com>
2017-08-18 09:17:49 -0400

    4.1 done

--- src/4-TreesAndGraphs/4.1.js
+++ src/4-TreesAndGraphs/4.1.js
@@ -1,13 +1,48 @@
 export function isConnected(graph, node1, node2){
     //do node1 first, then node2
-    bfs(graph, node1, node2)
+    if(bfs(graph, node1, node2)){
+        return true
+    }
+
+    return bfs(graph, node2, node1)
 }
 
 function bfs(graph, node1, node2){
     
-    //queue all node 1 adjacent nodes
+    //check if given nodes are the same
+    if(areEqual(node1, node2)){
+        return true
+    } 
     
-    console.log(graph.getNode(node1))
+    let q = new Queue
+
+    //queue first node
+    q.enqueue(node1.id)
+
+
+    while(q.queue.length > 0){
+        let currNode = graph.getNode(q.dequeue())
+        if(currNode == node2){
+            return true
+        }
+
+         //queue all node 1 adjacent nodes
+        currNode.adjacency.forEach( id => {
+            q.enqueue(id)
+        })
+    }
+
+    //loop through q until empty
+        //dequeue a node
+        //check if the node is equal to node 2
+            //if it is, return true
+        //queue all adjacent nodes
+
+    return false
+}
+
+function areEqual(node1, node2){
+    return node1 == node2
 }
 
 class Queue {
@@ -16,11 +51,12 @@ class Queue {
         this.queue = []
     }
     
-    queue(node){
-        this.queue.shift(node)
+    enqueue(value){
+
+        this.queue.push(value)
     }
 
-    enqueue(){
-        return this.queue.unshift()
+    dequeue(){
+        return this.queue.shift()
     }
 }
